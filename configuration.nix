83a138bbe06010104a42a35d7923394fea5844e3
zhouzhinian <77629397+zhouzhinian@users.noreply.github.com>
2025-02-27 11:01:14 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
   };
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
