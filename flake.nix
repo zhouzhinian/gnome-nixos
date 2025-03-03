c83283ee60f82a570afba37e6e5cbc1d1d0fa425
zhouzhinian <77629397+zhouzhinian@users.noreply.github.com>
2025-03-03 10:53:05 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    yazi.url = "github:sxyazi/yazi";	
     zen-browser.url = "github:youwen5/zen-browser-flake";	
   };
 
