f3290efd80f77105e026bb39d3140e23f13daeeb
zhouzhinian <77629397+zhouzhinian@users.noreply.github.com>
2025-03-03 10:56:46 +0800

    Update user.nix

--- user.nix
+++ user.nix
@@ -6,36 +6,41 @@
    environment.systemPackages = with pkgs; [
    	git wget ripgrep jq yq-go eza fzf which file gnused zstd gnupg aria2 
 	tree zip xz unzip p7zip	clash-verge-rev sublime4
-	ollama rustup ghostty fastfetch yesplaymusic htop	
-	inputs.yazi.packages.${pkgs.system}.default
+	ollama rustup ghostty fastfetch htop 
+	papirus-icon-theme
+        vlc
+        mint-cursor-themes
+      	subversion
+ 	dracula-icon-theme
+    	dracula-theme
+	qbittorrent-enhanced
+	tokyonight-gtk-theme
+	gruvbox-gtk-theme
+	rose-pine-gtk-theme
+	rose-pine-icon-theme
+	qogir-icon-theme
+	arc-icon-theme
+	thunderbird
+	obs-studio
+	vscode
 	inputs.zen-browser.packages."${system}".default
   ];
-    programs.zsh.enable = true;
-   users.defaultUserShell = pkgs.zsh;
+programs.zsh.enable = true;
+users.defaultUserShell = pkgs.zsh;
    
  ## 虚拟机
-	programs.virt-manager.enable = true;
-	virtualisation.libvirtd.enable = true;
-	virtualisation.spiceUSBRedirection.enable = true;
+programs.virt-manager.enable = true;
+virtualisation.libvirtd.enable = true;
+virtualisation.spiceUSBRedirection.enable = true;
  ## Flatpak
- services.flatpak.enable = true;  
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  xdg.portal.config.common.default = "gtk";
+services.flatpak.enable = true;  
+xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+xdg.portal.config.common.default = "gtk";
    
 #   programs.clash-verge.package = "clash-verge-rev";
-   programs.clash-verge.autoStart = true;	
-#   programs.clash-verge.enable = true;		
-   services.ollama.enable = true;
-
-
-
-
+programs.clash-verge.autoStart = true;	
+programs.clash-verge.enable = true;		
+services.ollama.enable = true;
 # sublime4
-  nixpkgs.config.permittedInsecurePackages = ["openssl-1.1.1w" ];
-
-
-
-
-
-
+nixpkgs.config.permittedInsecurePackages = ["openssl-1.1.1w" ];
 }
